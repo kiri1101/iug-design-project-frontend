91a4b3e857fbee6956000d75b03d66c10eebafe6
kiri1101 <icejordan0@gmail.com>
2025-10-26 00:36:29 +0100

    fix: update api url in .env before building

--- .output/server/chunks/build/styles.mjs
+++ .output/server/chunks/build/styles.mjs
@@ -6,10 +6,10 @@ const styles = {
   "pages/signup.vue?vue&type=style&index=0&scoped=d2727363&lang.css": () => import('./signup-styles.ylUGPZn5.mjs').then(interopDefault),
   "../node_modules/nuxt/dist/app/components/error-404.vue": () => import('./error-404-styles.DHhrBApj.mjs').then(interopDefault),
   "../node_modules/nuxt/dist/app/components/error-500.vue": () => import('./error-500-styles.bha0c7fC.mjs').then(interopDefault),
-  "components/Button/OutlineButton.vue": () => import('./OutlineButton-styles.BK2uH83A.mjs').then(interopDefault),
-  "components/Nav/Breadcrumb.vue": () => import('./Breadcrumb-styles.D4ILX2un.mjs').then(interopDefault),
   "../node_modules/nuxt/dist/app/components/error-500.vue?vue&type=style&index=0&scoped=d08fec65&lang.css": () => import('./error-500-styles.bha0c7fC.mjs').then(interopDefault),
   "../node_modules/nuxt/dist/app/components/error-404.vue?vue&type=style&index=0&scoped=dec70bd4&lang.css": () => import('./error-404-styles.DHhrBApj.mjs').then(interopDefault),
+  "components/Button/OutlineButton.vue": () => import('./OutlineButton-styles.BK2uH83A.mjs').then(interopDefault),
+  "components/Nav/Breadcrumb.vue": () => import('./Breadcrumb-styles.D4ILX2un.mjs').then(interopDefault),
   "components/Button/OutlineButton.vue?vue&type=style&index=0&scoped=5d519cf0&lang.css": () => import('./OutlineButton-styles.BK2uH83A.mjs').then(interopDefault),
   "components/Nav/Breadcrumb.vue?vue&type=style&index=0&scoped=5a2ebd3f&lang.css": () => import('./Breadcrumb-styles.D4ILX2un.mjs').then(interopDefault)
 };
